d7aa4e81aa702608b30efe750a1e9e37991831fd
tor86 <bartley.victoria1@gmail.com>
2022-07-09 15:53:59 -0500

    Fixed

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,16 +4,6 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//Add a marker to the map for Los Angeles, California
-// first added this marker, then changed for circleMarker.. let marker = L.marker([34.0522, -118.2437]).addTo(map);
-//Created light yellow circle with balck lines w/300 pixel radius 
-//L.circleMarker([34.052, -118.2437], {
-   // radius: 300,
-    //color: "black",
-    //fillColor: '#ffffa1'
-//}).addTo(map);
-
-
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
