9cdd7088be29571d1a561323a35c780202ab4bb0
tor86 <bartley.victoria1@gmail.com>
2022-07-09 15:47:18 -0500

    update

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -7,11 +7,11 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //Add a marker to the map for Los Angeles, California
 // first added this marker, then changed for circleMarker.. let marker = L.marker([34.0522, -118.2437]).addTo(map);
 //Created light yellow circle with balck lines w/300 pixel radius 
-L.circleMarker([34.052, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
-}).addTo(map);
+//L.circleMarker([34.052, -118.2437], {
+   // radius: 300,
+    //color: "black",
+    //fillColor: '#ffffa1'
+//}).addTo(map);
 
 
 
