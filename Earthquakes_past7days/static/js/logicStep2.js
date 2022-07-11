d679a37b5362db208bf9637c07acc08f15053140
tor86 <bartley.victoria1@gmail.com>
2022-07-11 16:39:03 -0500

    Earthquake/Style

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -31,10 +31,46 @@ let map = L.map('mapid', {
 //Pass map layers into layer control and add the layer control to the map
 L.control.layers(baseMaps).addTo(map);
 
+
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+
+
+// Returns the style data for each of the earthquakes on the map
+// Pass the magnitude of the earthquake into a function
+// to calculate radius
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
+//Determines the radius of earthquake marker based on magnitude
+//If magnitude of 0 will be plotted with radius of 1
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+
+// Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+
+// We turn each feature into a circleMarker on the map.
+
+pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+      //Set style for each circleMarker
+      style: styleInfo
+    }).addTo(map);
 });
-
-
